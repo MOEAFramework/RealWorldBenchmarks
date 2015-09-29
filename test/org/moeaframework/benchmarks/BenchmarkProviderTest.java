82ef88335a4b42af0e5eacc67d0a60a13d4fd728
David Hadka <dmh309@arl.psu.edu>
2015-09-29 16:14:40 -0400

    Adds testing code
    Adds Ant scripts for testing, Travis CI configuration file,
    and unit tests.

---
+++ test/org/moeaframework/benchmarks/BenchmarkProviderTest.java
@@ -0,0 +1,66 @@
+package org.moeaframework.benchmarks;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.moeaframework.Executor;
+import org.moeaframework.core.spi.ProblemFactory;
+
+/**
+ * Tests to ensure each benchmark problem can be instantiated with the MOEA
+ * Framework.
+ */
+public class BenchmarkProviderTest {
+	
+	protected void test(String problemName) {
+		new Executor()
+				.withAlgorithm("NSGAII")
+				.withProblem(problemName)
+				.run();
+
+		Assert.assertNotNull(
+				ProblemFactory.getInstance().getReferenceSet(problemName));
+	}
+	
+	@Test
+	public void testCarSideImpact() {
+		test("CarSideImpact");
+	}
+	
+	@Test
+	public void testElectricMotor() {
+		test("ElectricMotor");
+	}
+	
+	@Test
+	public void testGAA() {
+		test("GAA");
+	}
+	
+	@Test
+	public void testHBV() {
+		test("HBV");
+	}
+	
+	@Test
+	public void testLakeProblem() {
+		test("LakeProblem");
+	}
+	
+	@Test
+	public void testLRGV() {
+		test("LRGV");
+	}
+	
+	@Test
+	@Ignore("requires matlab")
+	public void testRadar() {
+		test("Radar");
+	}
+	
+	@Test
+	public void testWDS() {
+		test("WDS(GOY)");
+	}
+
+}
