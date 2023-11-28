5fe624aa1d7b51ddae1ca35cea96e6823b2bcdd8
David Hadka <dhadka@users.noreply.github.com>
2023-11-28 01:11:49 +0000

    Fix compilation of WDS

--- native/WDS/src/models.h
+++ native/WDS/src/models.h
@@ -1,5 +1,5 @@
-int nvars;
-int nobjs;
-int nconstrs;
-char* inputFile;
+extern int nvars;
+extern int nobjs;
+extern int nconstrs;
+extern char* inputFile;
 void test_problem (double *xreal, double *xbin, int **gene, double *obj, double *constr);
