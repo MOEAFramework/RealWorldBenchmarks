ae182dfce6536cdc5676f534bda5a77278d33c05
David Hadka <dhadka@users.noreply.github.com>
2023-12-31 17:23:45 +0000

    Exit immediately

--- native/Radar/bin/startEval.m
+++ native/Radar/bin/startEval.m
@@ -1,6 +1,7 @@
 function [] = startEval(nvars, nobjs, ncons, functionHandle, port)
 
 disp('Starting...');
+exit
 
 if ~libisloaded('libmoeaframework')
     loadlibrary('libmoeaframework', 'moeamatlab.h');
