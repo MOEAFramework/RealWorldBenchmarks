84d148c30e755b44e8cdb289e597cff9bf39a732
David Hadka <dhadka@users.noreply.github.com>
2023-12-31 17:41:01 +0000

    Choose correct lib name

--- native/Radar/bin/startEval.m
+++ native/Radar/bin/startEval.m
@@ -1,7 +1,13 @@
 function [] = startEval(nvars, nobjs, ncons, functionHandle, port)
 
-if ~libisloaded('moeaframework')
-    loadlibrary('moeaframework', 'moeamatlab.h');
+if ispc
+    libname = 'moeaframework'
+else
+    libname = 'libmoeaframework'
+end
+
+if ~libisloaded(libname)
+    loadlibrary(libname, 'moeamatlab.h');
 end
 
 if (isa(functionHandle, 'function_handle'))
@@ -17,22 +23,22 @@ if (nargin < 5)
 end
 
 disp('Waiting for connection...');
-calllib('moeaframework', 'MOEA_Init_socket', nobjs, ncons, port);
+calllib(libname, 'MOEA_Init_socket', nobjs, ncons, port);
 disp('Connection established...');
 
-while strcmp(calllib('moeaframework', 'MOEA_Next_solution'), 'MOEA_SUCCESS')
+while strcmp(calllib(libname, 'MOEA_Next_solution'), 'MOEA_SUCCESS')
     vars = zeros(1, nvars);
     pvars = libpointer('doublePtr', vars);
-    calllib('moeaframework', 'MOEA_Read_doubles', nvars, pvars);
+    calllib(libname, 'MOEA_Read_doubles', nvars, pvars);
     vars = get(pvars, 'Value');
 
     [objs, cons] = functionHandle(vars);
 
     pobjs = libpointer('doublePtr', objs);
     pcons = libpointer('doublePtr', cons);
-    calllib('moeaframework', 'MOEA_Write', pobjs, pcons);
+    calllib(libname, 'MOEA_Write', pobjs, pcons);
 end
 
-calllib('moeaframework', 'MOEA_Terminate');
+calllib(libname, 'MOEA_Terminate');
 disp('Shutting down...');
 exit
